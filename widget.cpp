63b5e6792f0f09126c6c5e6ef3701b6f6e95e0ee
kakunge <kakunge3@naver.com>
2023-08-06 15:53:44 +0900

    Add widget.*

---
+++ widget.cpp
@@ -0,0 +1,112 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QMessageBox>
+#include <QString>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    ui->pbCoffee->setEnabled(false);
+    ui->pbTea->setEnabled(false);
+    ui->pbMilk->setEnabled(false);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::changeEnabled()
+{
+    if (100 <= money)
+        ui->pbCoffee->setEnabled(true);
+    else
+        ui->pbCoffee->setEnabled(false);
+
+    if (150 <= money)
+        ui->pbTea->setEnabled(true);
+    else
+        ui->pbCoffee->setEnabled(false);
+
+    if (200 <= money)
+        ui->pbMilk->setEnabled(true);
+    else
+        ui->pbCoffee->setEnabled(false);
+}
+
+void Widget::changeMoney(int diff)
+{
+    money += diff;
+    changeEnabled();
+    ui->lcdNumber->display(money);
+}
+
+void Widget::on_pb10_clicked()
+{
+    changeMoney(10);
+}
+
+
+void Widget::on_pb50_clicked()
+{
+    changeMoney(50);
+}
+
+
+void Widget::on_pb100_clicked()
+{
+    changeMoney(100);
+}
+
+
+void Widget::on_pb500_clicked()
+{
+    changeMoney(500);
+}
+
+
+void Widget::on_pbCoffee_clicked()
+{
+    changeMoney(-100);
+}
+
+
+void Widget::on_pbTea_clicked()
+{
+    changeMoney(-150);
+}
+
+
+void Widget::on_pbMilk_clicked()
+{
+    changeMoney(-200);
+}
+
+
+void Widget::on_pbReset_clicked()
+{
+    QMessageBox mb;
+    QString text;
+    int n500;
+    int n100;
+    int n50;
+    int n10;
+
+    n500 = money/500;
+    money -= 500 * n500;
+    n100 = money/100;
+    money -= 100 * n100;
+    n50 = money/50;
+    money -= 50 * n50;
+    n10 = money/10;
+    money -= 10 * n10;
+
+    text = QString("500 : %1 100 : %2 50 : %3 10 : %4").arg(n500).arg(n100).arg(n50).arg(n10);
+
+    mb.information(nullptr, "Title", text);
+
+    changeMoney(-money);
+}
+
